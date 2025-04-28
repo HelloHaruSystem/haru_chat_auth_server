1df693c53c3664e807c9865fb19cbf000c90f4cf
Halfdan <halfdansc@gmail.com>
2025-04-28 13:44:19 +0200

    created the User class

--- models/User.js
+++ models/User.js
@@ -1,1 +1,81 @@
 // user model definition goes here
+class User {
+    constructor(id, username, password, createdAt, isBanned) {
+        if (typeof id !== "number") {
+            throw new Error("id must be a number");
+        }
+        this._id = id;
+
+        if (typeof username !== "string" && username.trim() === "") {
+            throw new Error("username must be a string and not empty");
+        }
+        this._username = username;
+
+        if (typeof password !== "string" && password.trim() === "") {
+            throw new Error("password can't be empty");
+        }
+        this._password = password; // this will be hashed before storage
+
+        if (!(createdAt instanceof Date)) {
+            throw new Error("createdAt must be a Date object");
+        }
+        this._createdAt = createdAt;
+
+        if (typeof isBanned !== "boolean") {
+            throw new Error("isBanned must be a boolean");
+        }
+        this._isBanned = isBanned;
+    }
+
+    getId() {
+        return this._id;
+    };
+
+    setId(id) {
+        if (typeof username !== "string" && username.trim() === "") {
+            throw new Error("username must be a string and not empty");
+        }
+        this._id = id;
+    };
+
+    getUsername() {
+        return this._username;
+    };
+
+    setUsername(username) {
+        if (typeof username !== "string" && username.trim() === "") {
+            throw new Error("username must be a string and not empty");
+        }
+        this._username = username;
+    };
+
+    // this needs to get hashed before storage
+    setPassword(password) {
+        this._password = password;
+    };
+
+    getCreatedAt() {
+        return this._createdAt;
+    };
+
+    getIsBanned() {
+        return this._isBanned;
+    };
+
+    setIsBanned(banned) {
+        if (typeof isBanned !== "boolean") {
+            throw new Error("isBanned must be a boolean");
+        }
+        this._isBanned = banned;
+    };
+
+    // returns user object without password
+    getSafeObject() {
+        return {
+            id: this._id,
+            username: this._username,
+            createdAt: this._createdAt,
+            isBanned: this._isBanned
+        };
+    };
+};
