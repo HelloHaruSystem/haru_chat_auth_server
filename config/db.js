6c8bb992d02a80b5027e793a38ae1339ba4f54ae
Halfdan <halfdansc@gmail.com>
2025-04-28 11:46:39 +0200

    made db connection possible

--- config/db.js
+++ config/db.js
@@ -1,1 +1,33 @@
 // db stuff
+import pg from "pg";
+import dotenv from "dotenv";
+
+const { Pool } = pg;
+
+// load environment variables
+dotenv.config();
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+});
+
+// test connection
+pool.query("SELECT NOW()", (err, res) => {
+    if (err) {
+        console.error("Error connecting to database: ", err);
+    } else {
+        console.log("Database connected successfully");
+    }
+    pool.end();
+});
+
+// query method
+const query = (text, params) => {
+    pool.query(text, params);
+};
+
+export { pool, query };
--- utils/jwtUtils.js
+++ utils/jwtUtils.js
@@ -1,1 +1,13 @@
 // JWT generation and validation utils goes here
+
+const generateToken = (payload) => {
+
+};
+
+const verifyToken = (token) => {
+
+};
+
+const decodeToken = (token) => {
+
+};
